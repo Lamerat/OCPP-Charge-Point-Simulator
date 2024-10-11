64ff5ad5b2b946eccf5700d9bc37a81fb7f80598
Simeon Mladenov <lamerkata@abv.bg>
2024-10-11 17:41:24 +0300

    Update

--- src/common/help-functions.js
+++ src/common/help-functions.js
@@ -1,8 +1,13 @@
 import moment from 'moment'
 import { customAlphabet  } from 'nanoid'
+import { connectors } from './constants'
 
 export const getId = () => customAlphabet ('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', 36)()
 
-export const saveSettings = (settings) => localStorage.setItem('OCPPSettings', JSON.stringify(settings))
+export const saveSettings = (settings) => {
+  localStorage.setItem('OCPPSettings', JSON.stringify(settings))
+  connectors[1].idTag = settings?.mainSettings.RFIDTag
+  connectors[2].idTag = settings?.mainSettings.RFIDTag
+}
 
 export const OCPPDate = (date) => moment(date).format('YYYY-MM-DDTHH:mm:ss').toString() + 'Z'
